198728b9c4f20dfa90900bf8a41d975ef44bd309
Evan Carlson <evanrc1@gmail.com>
2024-03-20 03:13:09 -0700

    added alt

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
     return (
         <div className="about-me">
             <div id="profile">
-                <img src= {profilePic} id="p-pic" />
+                <img src= {profilePic} id="p-pic" alt="Profile" />
             </div>
             <p>Hi I'm Evan, A junior web developer newly graduated from UC Davis's Coding Bootcamp. I am trained in full stack web-development but currently enjoy the creativity that comes with front-end work.
                 Please stay a while and browse what projects I have to show currently.</p>
