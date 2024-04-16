7b32edee0260a64b7807ca30f878ebb7ce5ee9b3
Evan Carlson <evanrc1@gmail.com>
2024-04-15 20:31:02 -0700

    changed bakground color

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -10,8 +10,7 @@ const AboutMe = () => {
             </div>
             <p>Hi I'm Evan, A junior web developer newly graduated from UC Davis's Coding Bootcamp. I am trained in full stack web-development but currently enjoy the creativity that comes with front-end work.
                 Please stay a while and browse what projects I have to show currently.</p>
-            <p>Outside of coding I am an avid gamer. I have a special love for The Halo and Elder Scrolls series. You might also find me at the billiards hall or on top of the mountain snowboarding depending on
-                the day of the week or time of year.</p>
+          
         </div>
     );
 };
